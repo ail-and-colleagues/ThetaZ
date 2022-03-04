906035764796b293a9b7114c1f3c056d27564cdc
hideki-yamaguchi <86000163+hideki-yamaguchi@users.noreply.github.com>
2022-03-04 14:53:51 +0900

    Add files via upload

--- conv_loop.sh
+++ conv_loop.sh
@@ -10,16 +10,15 @@ while read -r line
 do
 	#作業デレクトリを画像保存先に変更
 	col1=`echo ${line} | cut -d ',' -f 1`
-	cd ${col1}
+	dir_data=`basename ${col1}`
+	cd ${DIRNAME}/${dir_data}
 	#dcrawによるtiff変換
 	dcraw -v -T -g 1 1 -W *.DNG
 	cd ${CDIR}
 	#pythonスクリプトでXYZ変換
-	python3 conv_xyz.py --input ${col1}
+	python3 conv_xyz.py --input ${DIRNAME}/${dir_data}
 done < ${DIRNAME}/dirList.txt
 
 
 #作業デレクトリをもとに戻す
 cd ${CDIR}
-
-
